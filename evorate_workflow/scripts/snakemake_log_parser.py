87ba36df063fa048a1adfa33a592b4ece1e64b80
gabepen <gabrielpenun@gmail.com>
2024-06-05 12:43:05 -0700

    slurm log support
    E value param for evorate workflow

--- evorate_workflow/scripts/snakemake_log_parser.py
+++ evorate_workflow/scripts/snakemake_log_parser.py
@@ -21,10 +21,31 @@ def parse_snakemake_log(log_file):
 
     return failed_samples
 
+def parse_snakemake_log_slurm(slurm_log_file):
+    failed_samples = {}
+    with open(slurm_log_file, 'r') as file:
+        for line in file:
+            match = re.search(r'Error in rule (.+)', line)
+            if match:
+                rule = match.group(1).strip(':')
+                status = 'NA'
+                for line in file:
+                    match = re.search(r"message:.*SLURM status is:\s+'(\w+)'", line)  # Modify the regex pattern to match the desired string even if it doesn't occur at the start of the line
+                    if match:
+                        status = match.group(1)
+                    output_match = re.search(r'output: (.+)', line)
+                    if output_match:
+                        sample = '_'.join(line.split('/')[-1].strip().split('_')[0:2])
+                        break
+                if sample not in failed_samples:
+                    failed_samples[sample] = (rule, status)
+
+    return failed_samples
 # Usage example
-log_file = '/storage1/gabe/mimic_screen/code/mimic_screen/evorate_workflow/.snakemake/log/2024-05-21T165934.414516.snakemake.log'
-failed_samples = parse_snakemake_log(log_file)
+log_file = '/private/groups/corbettlab/gabe/mimic_screen/evorate_workflow/.snakemake/log/2024-05-29T162611.929951.snakemake.log'
+failed_samples = parse_snakemake_log_slurm(log_file)
 
 print(f"Number of failed samples: {len(failed_samples)}")
 for sample, meta in failed_samples.items():
-    print(f"Sample: {sample}, Failed at Rule: {meta[0]} JobID: {meta[1]}")
+    print(f"Sample: {sample} Rule: {meta[0]} Status: {meta[1]}")
+print(f"Total number of failed samples: {len(failed_samples)}")
