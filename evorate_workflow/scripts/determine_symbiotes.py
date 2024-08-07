9a129ed6c855e8e652fbb88ea12dc30a7906f17c
gabepen <gabrielpenun@gmail.com>
2024-08-07 12:56:27 -0700

    bv-brc api calls for determining infectious status

--- evorate_workflow/scripts/determine_symbiotes.py
+++ evorate_workflow/scripts/determine_symbiotes.py
@@ -2,7 +2,8 @@ from tqdm import tqdm
 import sys
 import os
 import subprocess
-import pdb
+import urllib.request
+import json
 
 def check_for_symbiosis(tax_id_list):
 
@@ -20,13 +21,29 @@ def check_for_symbiosis(tax_id_list):
             print(f"Error with {tax_id}")
             print(result.stdout)
             continue
-       
-
 
     return symbiotes
 
+def check_for_infectious(tax_id_list):
+    
+    infectious = []
+    total_non_infectious = 0
+    for tax_id in tax_id_list:
+        tax_id = tax_id.strip()
+        response = urllib.request.urlopen(f'https://www.bv-brc.org/api/taxonomy/?eq(taxon_id,{tax_id})&http_accept=application/json')
+        html = response.read()
+        json_string = html.decode("utf-8")
+        json_array = json.loads(json_string)
+        try:
+            if 'symbiont' not in json_array[0]['taxon_name']:
+                if json_array[0]['genomes'] == 0:
+                    total_non_infectious += 1
+                    print(f"{tax_id} is not infectious")
+        except IndexError:
+            total_non_infectious += 1
+    print(f"Total non-infectious: {total_non_infectious}")   
 '''
-Usage: python snakemake_log_parser.py log_file output_file rule_spec
+Usage: python determine_symbiotes.py tax_ids output_file
     tax_ids: path to genomes_selected file
     output_file: path to output file for sample
 '''
@@ -37,7 +54,8 @@ output_file = sys.argv[2]
 with open(tax_ids) as f:
     tax_id_list = f.readlines()
 
-symbiotes = check_for_symbiosis(tax_id_list)
+#symbiotes = check_for_symbiosis(tax_id_list)
+infectious_bacs = check_for_infectious(tax_id_list)
 
 with open(output_file, 'w+') as f:
     for tax_id in symbiotes:
